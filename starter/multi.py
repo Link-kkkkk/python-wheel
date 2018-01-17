fe550f2965fdf07a12ba291d92f9daadab4589bd
Link <13788485679@sina.cn>
2018-01-17 18:09:21 +0800

    多进程bug解决

---
+++ starter/multi.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+#coding:utf-8
+
+import os
+
+print('Process (%s) start...' % os.getpid())
+
+# fork()调用一次，返回两次，因为操作系统自动把当前进程（称为父进程）复制了一份（称为子进程），然后，分别在父进程和子进程内返回。
+# 子进程永远返回0，而父进程返回子进程的ID
+pid = os.fork()
+
+# if pid == 0:
+#     print('I am child process (%s) and my parent is %s.' % (os.getpid(), os.getppid()))
+# else:
+#     print('I (%s) just created a child process (%s).' % (os.getpid(), pid))
+
+from multiprocessing import Process
+import os
+
+# 子进程要执行的代码
+def run_proc(name):
+    print('Run child process %s (%s)...' % (name, os.getpid()))
+
+if __name__=='__main__':
+    print('Parent process %s.' % os.getpid())
+    p = Process(target=run_proc, args=('test',))
+    print('Child process will start.')
+    p.start()
+    p.join()
+    print('Child process end.')
